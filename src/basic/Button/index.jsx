5dbf0f4877f8f946c9e7490e17ab8bbfb5f99eb8
yunjjeongjo <two6885@gmail.com>
2022-10-04 19:23:13 +0900

    feat: Button 컴포넌트 추가

---
+++ src/basic/Button/index.jsx
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  display: block;
+  width: 100%;
+  height: 40px;
+  padding: 8px 6px;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  outline: none;
+  background-color: black;
+  box-sizing: border-box;
+  cursor: pointer;
+  &:hover {
+    background-color: #444;
+  }
+  &:active {
+    background-color: #666;
+  }
+  &:disabled {
+    background-color: #888;
+  }
+`;
+
+export default Button;
