bc67c6c01abe58b07d3bcf6d07b33488fc7ee412
yunjjeongjo <two6885@gmail.com>
2022-10-05 15:35:40 +0900

    feat: SignupForm 구현

---
+++ src/components/SignupForm/index.js
@@ -0,0 +1,42 @@
+import Input from "../../basic/Input";
+import Button from "../../basic/Button";
+import useForm from "../../pages/hooks/useForm";
+import CardForm from "../CardForm";
+
+const SignupForm = ({ onSubmit }) => {
+  const { errors, isLoading, handleChange, handleSubmit } = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit,
+    validate: ({ email, password }) => {
+      const newErrors = {};
+      if (!email) newErrors.name = "이름을 입력해주세요.";
+      if (!password) newErrors.password = "비밀번호를 입력해주세요.";
+      return newErrors;
+    },
+  });
+
+  return (
+    <CardForm onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        name="email"
+        placeholder="이메일을 입력해 주세요"
+        onChange={handleChange}
+      ></Input>
+      <Input
+        type="password"
+        name="password"
+        placeholder="비밀번호를 입력해 주세요"
+        onChange={handleChange}
+      ></Input>
+      <Button type="submit" disabled={isLoading}>
+        회원가입
+      </Button>
+    </CardForm>
+  );
+};
+
+export default SignupForm;
