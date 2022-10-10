9776ade70c94b372eeda1538d09862d4902255f7
yunjjeongjo <two6885@gmail.com>
2022-10-10 22:46:38 +0900

    fix: 오타 수정

--- src/utils/api/auth.js
+++ src/utils/api/auth.js
@@ -3,26 +3,24 @@ import { setItem } from "../storage";
 
 export const onLogin = async ({ email, password }) => {
   // JWT 토큰 및 로컬 스토리지 초기화
-  setItem("ACESS_TOKEN", "");
+  setItem("ACCESS_TOKEN", "");
   localStorage.clear();
-  const { data, error } = await login({ email, password });
-  console.log(data, error);
+  const { data } = await login({ email, password });
   if (data) {
-    console.log(data);
-    setItem("ACESS_TOKEN", data.access_token); // 로그인 성공 시, JWT 토큰 갱신
+    setItem("ACCESS_TOKEN", data.access_token); // 로그인 성공 시, JWT 토큰 갱신
+  } else {
   }
   return data;
 };
 
 export const onSignup = async ({ email, password }) => {
   // JWT 토큰 및 로컬 스토리지 초기화
-  setItem("ACESS_TOKEN", "");
+  setItem("ACCESS_TOKEN", "");
   localStorage.clear();
-  const { data, error } = await signup({ email, password });
-  console.log(data, error);
+  const { data } = await signup({ email, password });
+
   if (data) {
-    console.log(data);
-    setItem("ACESS_TOKEN", data.access_token); // 회원가입 성공 시, JWT 토큰 갱신
+    setItem("ACCESS_TOKEN", data.access_token); // 회원가입 성공 시, JWT 토큰 갱신
   }
   return data;
 };
