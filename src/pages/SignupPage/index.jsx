b66ef624fa086f36dca41a2a70308dbfec422f84
yunjjeongjo <two6885@gmail.com>
2022-10-10 22:48:20 +0900

    feat: 회원가입 페이지 구현

--- src/pages/SignupPage/index.jsx
+++ src/pages/SignupPage/index.jsx
@@ -1,5 +1,27 @@
+import SignupForm from "../../components/SignupForm";
+import { onSignup } from "../../utils/api/auth";
+import { Link, useNavigate } from "react-router-dom";
+
 const SignupPage = () => {
-  return <>Signup Page</>;
+  const navigate = useNavigate();
+  const handleSubmit = async ({ email, password }) => {
+    try {
+      await onSignup({
+        email,
+        password,
+      });
+      navigate(`/todo`);
+    } catch (e) {
+      alert(e.response.data.message);
+      console.log(e);
+    }
+  };
+  return (
+    <>
+      <SignupForm onSubmit={handleSubmit}></SignupForm>
+      <Link to="/">계정이 있으신가요? 로그인</Link>
+    </>
+  );
 };
 
 export default SignupPage;
