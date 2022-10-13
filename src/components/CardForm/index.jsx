f37499bb0f856352ed858b1612b4a212367f827c
yunjjeongjo <two6885@gmail.com>
2022-10-14 01:26:51 +0900

    design: CardForm 컴포넌트 CSS 변경

--- src/components/CardForm/index.jsx
+++ src/components/CardForm/index.jsx
@@ -1,8 +1,14 @@
 import styled from "@emotion/styled";
 
 const CardForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
   padding: 16px;
   width: 400px;
+  height: 450px;
   background-color: white;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
   box-sizing: border-box;
