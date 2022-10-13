8d2998026a2567c73eacc402f2a8f8b6e46ca688
yunjjeongjo <two6885@gmail.com>
2022-10-14 01:31:15 +0900

    feat: MainPage 구현

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,5 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { getItem } from "../../utils/storage";
+import Text from "../../basic/Text";
+import TodoForm from "../../components/TodoForm";
+import TodoList from "../../components/TodoList";
+
 const MainPage = () => {
-  return <>Main Page</>;
+  const navigate = useNavigate();
+  const token = getItem("ACCESS_TOKEN", null);
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/", { replace: true });
+    }
+  }, []);
+
+  return (
+    <>
+      <Text size={30} color={"#555555"}>
+        TODO LIST
+      </Text>
+      <TodoForm></TodoForm>
+      <TodoList></TodoList>
+    </>
+  );
 };
 
 export default MainPage;
