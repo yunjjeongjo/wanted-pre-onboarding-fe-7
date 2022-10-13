9df38f5c9047f32297fad5ae780f0ec2f85334de
yunjjeongjo <two6885@gmail.com>
2022-10-14 01:29:17 +0900

    feat: TodoList 컴포넌트 구현

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import TodoItem from "../TodoItem";
+import { useTodos } from "../../contexts/TodoProvider";
+import { useEffect, useState } from "react";
+import { getItem } from "../../utils/storage";
+
+const UnorderedList = styled.ul`
+  width: 400px;
+  margin: 0;
+  padding: 0;
+  & > li {
+    &:not(:first-child) {
+      margin-top: 12px;
+    }
+  }
+`;
+
+const TodoList = (props) => {
+  const { todos, getTodos } = useTodos();
+  const token = getItem("ACCESS_TOKEN", null);
+
+  useEffect(() => {
+    if (token) {
+      getTodos(token);
+    }
+  }, []);
+
+  return (
+    <UnorderedList {...props}>
+      {todos.map((item) => {
+        return (
+          <TodoItem
+            key={item.id}
+            id={item.id}
+            content={item.todo}
+            complete={item.isCompleted}
+          />
+        );
+      })}
+    </UnorderedList>
+  );
+};
+
+export default TodoList;
