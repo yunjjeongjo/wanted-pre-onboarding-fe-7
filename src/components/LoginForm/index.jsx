8ffcebc8447932664c9fb3a0550b7ae5a964985b
yunjjeongjo <two6885@gmail.com>
2022-10-14 01:30:15 +0900

    design: LoginForm 컴포넌트 CSS 수정

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -2,6 +2,48 @@ import Input from "../../basic/Input";
 import Button from "../../basic/Button";
 import useForm from "../../pages/hooks/useForm";
 import CardForm from "../CardForm";
+import styled from "@emotion/styled";
+import Text from "../../basic/Text";
+import { Link } from "react-router-dom";
+
+const Title = styled(Text)`
+  margin-bottom: 40px;
+`;
+
+const StyledInput = styled(Input)`
+  height: 50px;
+  width: 300px;
+  padding-left: 15px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+  border: none;
+  background-color: #f1f0f5;
+  color: #666666;
+  font-size: 15px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  height: 50px;
+  width: 300px;
+  margin-top: 20px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-size: 15px;
+`;
+
+const LinkText = styled(Link)`
+  text-decoration: none;
+  color: #666666;
+  font-size: 15px;
+  &:hover {
+    color: #222222;
+  }
+`;
 
 const LoginForm = ({ onSubmit }) => {
   const { errors, isLoading, handleChange, handleSubmit } = useForm({
@@ -20,21 +62,25 @@ const LoginForm = ({ onSubmit }) => {
 
   return (
     <CardForm onSubmit={handleSubmit}>
-      <Input
+      <Title size={30} color={"#555555"}>
+        LOG IN
+      </Title>
+      <StyledInput
         type="text"
         name="email"
         placeholder="이메일을 입력해 주세요"
         onChange={handleChange}
-      ></Input>
-      <Input
+      ></StyledInput>
+      <StyledInput
         type="password"
         name="password"
         placeholder="비밀번호를 입력해 주세요"
         onChange={handleChange}
-      ></Input>
-      <Button type="submit" disabled={isLoading}>
+      ></StyledInput>
+      <StyledButton type="submit" disabled={isLoading}>
         로그인
-      </Button>
+      </StyledButton>
+      <LinkText to="/signup">계정이 없으신가요? 회원가입</LinkText>
     </CardForm>
   );
 };
