b2edebc4684c4a24b56ba82625d843b698f9ee64
yunjjeongjo <two6885@gmail.com>
2022-10-14 01:27:11 +0900

    feat: TodoProvider 구현

---
+++ src/contexts/TodoProvider.js
@@ -0,0 +1,46 @@
+import { createContext, useContext } from "react";
+import useLocalStorage from "../pages/hooks/useLocalStorage";
+import {
+  getTodo,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} from "../utils/api/todoApi";
+
+const TodoContext = createContext();
+export const useTodos = () => useContext(TodoContext);
+
+const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useLocalStorage("todos", []);
+
+  const getTodos = async (token) => {
+    const data = await getTodo(token);
+    setTodos(data.data);
+  };
+
+  const createTodos = async (token, content) => {
+    const data = await createTodo(token, content);
+    setTodos([...todos, data.data]);
+  };
+
+  const updateTodos = async (id, status, content, token) => {
+    console.log(id, status, content, token);
+    const data = await updateTodo(id, token, content, status);
+    setTodos(todos.map((item) => (item.id === id ? data.data : item)));
+  };
+
+  const removeTodo = async (token, id) => {
+    await deleteTodo(token, id);
+    setTodos(todos.filter((item) => item.id !== id));
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{ todos, updateTodos, removeTodo, createTodos, getTodos }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
