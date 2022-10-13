4daa76c6cd5acd087147248bbdd92021a90f3a0b
yunjjeongjo <two6885@gmail.com>
2022-10-14 01:30:27 +0900

    design: SignupForm 컴포넌트 CSS 수정

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -2,6 +2,63 @@ import Input from "../../basic/Input";
 import Button from "../../basic/Button";
 import useSignupForm from "../../pages/hooks/useSignupForm";
 import CardForm from "../CardForm";
+import { Link } from "react-router-dom";
+import styled from "@emotion/styled";
+import Text from "../../basic/Text";
+
+const Title = styled(Text)`
+  margin-bottom: 40px;
+`;
+
+const StyledInput = styled(Input)`
+  position: relative;
+  height: 50px;
+  width: 300px;
+  padding-left: 15px;
+  border: none;
+  border-radius: 10px;
+  background-color: #f1f0f5;
+  color: #666666;
+  font-size: 15px;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  height: 50px;
+  width: 300px;
+  margin-top: 20px;
+  margin-bottom: 15px;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  font-size: 15px;
+`;
+
+const LinkText = styled(Link)`
+  text-decoration: none;
+  color: #666666;
+  font-size: 15px;
+  &:hover {
+    color: #222222;
+  }
+`;
+
+const ErrorText = styled(Text)`
+  position: relative;
+  left: -75px;
+  top: 3px;
+  color: red;
+  font-size: 12px;
+  margin-bottom: 15px;
+`;
+
+const SuccessText = styled(Text)`
+  color: white;
+  font-size: 12px;
+  margin-bottom: 15px;
+`;
 
 const SignupForm = ({ onSubmit }) => {
   const { handleChange, handleSubmit, emailError, passwordError } =
@@ -21,23 +78,35 @@ const SignupForm = ({ onSubmit }) => {
 
   return (
     <CardForm onSubmit={handleSubmit}>
-      <Input
+      <Title size={30} color={"#555555"}>
+        SIGN UP
+      </Title>
+      <StyledInput
         type="text"
         name="email"
         placeholder="이메일을 입력해 주세요"
         onChange={handleChange}
-      ></Input>
-      {emailError && "이메일 에러"}
-      <Input
+      ></StyledInput>
+      {emailError ? (
+        <ErrorText>*이메일 형식이 아닙니다</ErrorText>
+      ) : (
+        <SuccessText>성공</SuccessText>
+      )}
+      <StyledInput
         type="password"
         name="password"
         placeholder="비밀번호를 입력해 주세요"
         onChange={handleChange}
-      ></Input>
-      {passwordError && "패스워드 에러"}
-      <Button type="submit" disabled={emailError || passwordError}>
+      ></StyledInput>
+      {passwordError ? (
+        <ErrorText>*8자 이상 입력해 주세요</ErrorText>
+      ) : (
+        <SuccessText>성공</SuccessText>
+      )}
+      <StyledButton type="submit" disabled={emailError || passwordError}>
         회원가입
-      </Button>
+      </StyledButton>
+      <LinkText to="/">계정이 있으신가요? 로그인</LinkText>
     </CardForm>
   );
 };
